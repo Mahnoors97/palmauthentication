8ddfc42fc21a2acb5e1a01f72651188c0ac39ad6
unknown <Wajahat@WAJAHAT-DEV.pralstp.org>
2019-06-29 17:31:58 +0500

    Modification in code

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask
-from flask import request
 import  json
 from keras.applications.mobilenet import MobileNet, preprocess_input
 from keras.preprocessing import image
 from keras.models import Model
 from keras.layers import Input
 import pickle
-import os
+import tensorflow as tf
 import numpy as np
+import base64
+import time
+import os
 
 
 dir_path=""
@@ -28,40 +29,60 @@ classifier_path = config["classifier_path"]
 test_path= config["test_path"]
 image_size = (224, 224)
 
+user_labels=np.load("user_labels.npy")
+
+print(user_labels.shape)
 
 def create_model():
    
     if model_name == "mobilenet":
       base_model = MobileNet(include_top=include_top, weights=weights, input_tensor=Input(shape=(224,224,3)), input_shape=(224,224,3))
-      base_model.summary()
-      model = Model(input=base_model.input, output=base_model.get_layer('custom').output)
+      #base_model.summary()
+      model = Model(input=base_model.input, output=base_model.layers[-1].output)
     else:
       base_model = None
 
     return model
 
 def predict_label(x):
-    model_new=create_model()
-    model_new.load_weights(dir_path+model_path+"0.1.h5")
-    classifier = pickle.load(open(classifier_path, 'rb'))
-    train_labels = os.listdir(train_path)
+    
     x= np.expand_dims(x, axis=0)
     x= preprocess_input(x)
-    feature = model_new.predict(x)
+    feature = app.model.predict(x)
+    #print(feature.shape)
     flat = feature.flatten()
+    #print(flat.shape)
     flat = np.expand_dims(flat, axis=0)
-    preds = classifier.predict(flat)
-    #prediction 	= train_labels[preds[0]]
-    return train_labels[preds[0]]
+    #print(flat.shape)
+    preds = app.classifier.predict(flat)
+    #print(preds)
+    print(preds[0])
+    prediction 	= user_labels[preds[0]]
+    return prediction
 
+
+from flask import Flask
+from flask import request
 
 app = Flask(__name__)
+
+
+@app.before_first_request
+def load_model_to_app():
+    # Load the model
+    
+    model_new=create_model()
+    model_new.load_weights(dir_path+model_path+"0.1.h5")
+    app.classifier = pickle.load(open(classifier_path, 'rb'))
+    app.model = model_new
+        
+    # Save the graph to the app framework.
+    app.graph = tf.get_default_graph()
 
 @app.route('/')
 def index():
     
-    return "<h1>Welcome to Contact Less PALM Authentication</h1>"
-	
+    return "Welcome to Contact Less PALM Authentication"
 
 @app.route('/authenticate',methods = ['POST', 'GET'])
 def authenticate():
@@ -69,15 +90,26 @@ def authenticate():
     print("Entered in authenticate method")
     json_string=request.get_json()
     loaded_json = json.loads(json.dumps(json_string))
-    img=loaded_json['image_Array']
-    #print("JSON String "+str(img))
     
-    #path = test_path + "/"+image_name
-    #img= image.load_img(path, target_size=image_size)
-    x = image.img_to_array(img)
+    imgdata = base64.b64decode(loaded_json['image_Array'])
     
-    #return "it is "+ predict_label(x)
-    return "JSON String "+ predict_label(x)
+    milliseconds = int(round(time.time() * 1000))
+    
+    filename = str(milliseconds)+'.jpg'  # I assume you have a way of picking unique filenames
+    with open(filename, 'wb') as f:
+        f.write(imgdata)
+        
+    
+    
+    graph = app.graph    
+    with graph.as_default():
+        img= image.load_img(filename, target_size=image_size)
+        x = image.img_to_array(img)
+        predict=predict_label(x)
+        os.remove(filename)
+        print(predict)
+        return "I think it is a "+predict
+    
 
 
 @app.route('/authenticateURL')
@@ -86,15 +118,13 @@ def authenticateURL():
     print("Input image name "+image_name)
     path = test_path + "/"+image_name
     img= image.load_img(path, target_size=image_size)
+    
     x = image.img_to_array(img)
     
-    return "I think it is a " + predict_label(x)
+    graph = app.graph    
+    with graph.as_default():
+        return "I think it is a " + str(predict_label(x))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
-
-	
-	
-if __name__ == '__main__':
-    app.run()
+if __name__ == '__main__':     
+    app.run(host='0.0.0.0')
--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from keras.preprocessing import image
 from keras.models import Model
 from keras.layers import Input
 import pickle
-import os
+import tensorflow as tf
 import numpy as np
+
 
 dir_path=""
 
@@ -25,37 +26,55 @@ classifier_path = config["classifier_path"]
 test_path= config["test_path"]
 image_size = (224, 224)
 
+user_labels=np.load("user_labels.npy")
+
+print(user_labels.shape)
 
 def create_model():
    
     if model_name == "mobilenet":
       base_model = MobileNet(include_top=include_top, weights=weights, input_tensor=Input(shape=(224,224,3)), input_shape=(224,224,3))
-      base_model.summary()
-      model = Model(input=base_model.input, output=base_model.get_layer('custom').output)
+      #base_model.summary()
+      model = Model(input=base_model.input, output=base_model.layers[-1].output)
     else:
       base_model = None
 
     return model
 
 def predict_label(x):
-    model_new=create_model()
-    model_new.load_weights(dir_path+model_path+"0.1.h5")
-    classifier = pickle.load(open(classifier_path, 'rb'))
-    train_labels = os.listdir(train_path)
+    
     x= np.expand_dims(x, axis=0)
     x= preprocess_input(x)
-    feature = model_new.predict(x)
+    feature = app.model.predict(x)
+    #print(feature.shape)
     flat = feature.flatten()
+    #print(flat.shape)
     flat = np.expand_dims(flat, axis=0)
-    preds = classifier.predict(flat)
-    #prediction 	= train_labels[preds[0]]
-    return train_labels[preds[0]]
+    #print(flat.shape)
+    preds = app.classifier.predict(flat)
+    #print(preds)
+    print(preds[0])
+    prediction 	= user_labels[preds[0]]
+    return prediction
 
 
 from flask import Flask
 from flask import request
 
 app = Flask(__name__)
+
+
+@app.before_first_request
+def load_model_to_app():
+    # Load the model
+    
+    model_new=create_model()
+    model_new.load_weights(dir_path+model_path+"0.1.h5")
+    app.classifier = pickle.load(open(classifier_path, 'rb'))
+    app.model = model_new
+        
+    # Save the graph to the app framework.
+    app.graph = tf.get_default_graph()
 
 @app.route('/')
 def index():
@@ -68,15 +87,13 @@ def authenticate():
     print("Entered in authenticate method")
     json_string=request.get_json()
     loaded_json = json.loads(json.dumps(json_string))
+    print(loaded_json)
     img=loaded_json['image_Array']
-    #print("JSON String "+str(img))
     
-    #path = test_path + "/"+image_name
-    #img= image.load_img(path, target_size=image_size)
-    x = image.img_to_array(img)
+    graph = app.graph    
+    with graph.as_default():
+        return "I think it is a " + str(predict_label(img))
     
-    #return "it is "+ predict_label(x)
-    return "JSON String "+ predict_label(x)
 
 
 @app.route('/authenticateURL')
@@ -87,8 +104,10 @@ def authenticateURL():
     img= image.load_img(path, target_size=image_size)
     x = image.img_to_array(img)
     
-    return "I think it is a " + predict_label(x)
+    graph = app.graph    
+    with graph.as_default():
+        return "I think it is a " + str(predict_label(x))
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':     
     app.run(host='0.0.0.0')
