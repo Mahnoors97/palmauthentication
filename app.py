f129c15394cd7d7f70f3cab6b27cc0ce39956060
Mahnoors97 <39123696+Mahnoors97@users.noreply.github.com>
2019-06-30 19:03:59 +0500

    Deleted the print statement from the code.

--- app.py
+++ app.py
@@ -115,8 +115,7 @@ def authenticate():
 @app.route('/authenticateURL')
 def authenticateURL():
     image_name = request.args.get('image_name')
-    print("Input image name "+image_name)
-    #path = test_path + "/"+image_name
+    path = test_path + "/"+image_name
     img= image.load_img(path, target_size=image_size)
     
     x = image.img_to_array(img)
